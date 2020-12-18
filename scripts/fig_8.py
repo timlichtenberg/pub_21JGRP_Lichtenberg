3f74a46a492f4a7f10ae173e2837280f8b718551
Tim Lichtenberg <lichtenberg.tim@gmail.com>
2020-12-18 10:29:33 +0000

    Figs 4+8 data

--- scripts/fig_8.py
+++ scripts/fig_8.py
@@ -5,6 +5,7 @@ from modules_plot import *
 from utils.modules_utils import *
 
 import matplotlib.gridspec as gridspec
+import json
 
 #====================================================================
 def plot_atmosphere( output_dir, sub_dirs ):
@@ -74,12 +75,9 @@ def plot_atmosphere( output_dir, sub_dirs ):
 
     
     data_file_photosphere = '../data/fig8_cff/photosphere.json'
-    if os.path.exists(data_file_photosphere):
-        a_file = open(data_file_photosphere, "r")
-        output = a_file.read()
-        print(output)
-    else:
-        photosphere = {}
+    with open(data_file_photosphere) as json_file:
+        photosphere = json.load(json_file)
+
 
     for ni, subdir in enumerate(sub_dirs):
 
@@ -107,211 +105,47 @@ def plot_atmosphere( output_dir, sub_dirs ):
 
         for nn, setting in enumerate( settings ):
 
-            if setting == "":
-                setting_name = "(a) Fiducial case"
-            if setting == "_mix":
-                setting_name = r"(b) Variable $\mathcal{L}$"
-            if setting == "_sep":
-                setting_name = r"(c) Solid–melt sep."
-            if setting == "_utbl":
-                setting_name = r"(d) Boundary layer"
-            if setting == "_sep_mix":
-                setting_name = r"Variable $\mathcal{L}$"+"\n"+"+ solid–melt sep."
-            if setting == "_sep_mix_utbl":
-                setting_name = r"Variable $\mathcal{L}$"+"\n"+"+ solid–melt sep."+"\n"+"+ UTBL"
-
-            data_dir = output_dir+"/"+subdir+setting
-
-            print(data_dir)
-
-            # Read in time sequences
-            data_times = su.get_all_output_times(data_dir)
-            keys = ( ('atmosphere','mass_liquid'),
-                       ('atmosphere','mass_solid'),
-                       ('atmosphere','mass_mantle'),
-                       ('atmosphere','mass_core'),
-                       ('atmosphere','temperature_surface'),
-                       ('atmosphere','emissivity'),
-                       ('rheological_front_phi','phi_global'),
-                       ('atmosphere','Fatm'),
-                       ('atmosphere','pressure_surface'),
-                       ('rheological_front_dynamic','depth'),
-                       ('rheological_front_dynamic','mesh_index')
-                       )
-            data = su.get_dict_surface_values_for_times( keys, data_times, data_dir )
-            mass_liquid         = data[0,:]
-            mass_solid          = data[1,:]
-            mass_mantle         = data[2,:]
-            mass_core           = data[3,:]
-            T_surf              = data[4,:]
-            emissivity          = data[5,:]
-            phi_global          = data[6,:]
-            Fatm                = data[7,:]
-            P_surf              = data[8,:]
-            rheol_front         = data[9,:]
-            rheol_front_idx     = data[10,:]
-
-            # FIND SPECIFIC TIMESTAMP
-
-            # Find critical parameter
-            RF_depth_crit = 0.01
-            # print(rheol_front/np.max(rheol_front))
-            RF_depth_crit_num, RF_depth_crit_idx = find_nearest(rheol_front/np.max(rheol_front), RF_depth_crit)
-            RF_depth_crit_time   = data_times[RF_depth_crit_idx]
-            
-            # RF_half_depth, RF_half_depth_idx = find_nearest(rheol_front, 0.5)
-            # RF_half_depth_time   = data_times[RF_half_depth_idx]
-            # Phi_global_intersect = phi_global[RF_half_depth_idx]
-            # print("RF:", RF_half_depth_idx, RF_half_depth, RF_half_depth_time, Phi_global_intersect)
-
-            phi_crit = 0.2
-            phi_crit_num, phi_crit_idx = find_nearest(phi_global, phi_crit)
-            phi_crit_time = data_times[phi_crit_idx]
-            # print("RF:", phi_crit_idx, RF_half_depth, RF_half_depth_time, Phi_global_intersect)
-
-            Fatm_crit = 1e+3
-            Fatm_crit_num, Fatm_crit_idx = find_nearest(Fatm, Fatm_crit)
-            Fatm_crit_time = data_times[Fatm_crit_idx]
-
-            # Define output time based on which criterion
-            if setting == "":
-                
-                output_time =  RF_depth_crit_time
-
-
-            # Find data_time closest to output_time wanted
-            atm_data_times = su.get_all_output_pkl_times(data_dir)
-            # print(atm_data_times)
-            time, time_idx = find_nearest(atm_data_times, output_time)
-
-            # Find planet mass and radius
-            myjson_o  = su.MyJSON( data_dir+'/{}.json'.format(data_times[0]) )
-            xx_radius = myjson_o.get_dict_values(['data','radius_b'])*1.0E-3
-            r_planet  = np.max(xx_radius*1e3) # m
-            core_mass   = myjson_o.get_dict_values(['atmosphere','mass_core'])
-            mantle_mass = myjson_o.get_dict_values(['atmosphere','mass_mantle'])
-            planet_mass = core_mass + mantle_mass
-
-            print(subdir, setting, output_time)
-
-
-            atm_file = data_dir+"/"+str(int(time))+"_atm.pkl"
-
-            # Read pickle file
-            atm_file_stream = open(atm_file,'rb')
-            atm = pkl.load(atm_file_stream)
-            atm_file_stream.close()
-
-            # color = fig_o.get_color( nn )
             color   = vol_colors[subdir][color_idx]
-          
-            dirs = {"output": output_dir, "rad_conv": "/Users/tim/bitbucket/pcd_couple-interior-atmosphere/atm_rad_conv"}
-            atm = atm_rad_conv.SocRadModel.radCompSoc(atm, dirs, recalc=False, calc_cf=True)
 
-            # Total CFF, normalized
-            cff_tot = atm.cff/np.sum(atm.cff)
-            prs     = atm.p/np.max(atm.p)
-
-            # Annotate height and pressure of CFF peak
-            # For H2 cut out lowermost peak
-            cut_idx = len(atm.p)
-            if subdir =="H2": cut_idx = -10
-            z_profile = AtmosphericHeight(atm, planet_mass, r_planet)*1e-3 # km
-            cff_max, cff_max_idx = find_nearest(cff_tot[:cut_idx], np.max(cff_tot[:cut_idx]))
-            print("HEIGHT_max, PRS, TMP:", cff_max, cff_max_idx, z_profile[cff_max_idx], "km", atm.p[cff_max_idx]/1e+5, "bar", atm.tmp[cff_max_idx], "K")
-
-            
-            # print "max" or "weighted"
-            output_label = "weighted"
-
-            # print(np.average(z_profile, weights=cff_tot))
-
-            # Weighted by CFF
-            if output_label == "weighted":
-                time_print  = latex_float(output_time)
-                z_print     = round(np.average(z_profile, weights=cff_tot))
-                prs_print   = round(np.average(atm.p/1e+5, weights=cff_tot),1)
-                tmp_print   = round(np.average(atm.tmp, weights=cff_tot))
-            # At max
-            if output_label == "max":
-                time_print  = latex_float(output_time)
-                z_print     = round(z_profile[cff_max_idx])
-                prs_print   = round(atm.p[cff_max_idx]/1e+5,1)
-                tmp_print   = round(atm.tmp[cff_max_idx])
-
-
-            # Label with time
-            label_a = vol_latex[subdir]+", "+latex_float(output_time)+" yr"
-            # Label w/o time
-            label_a = vol_latex[subdir]
-            # Label with time, height, pressure and temperature
-            label_a = vol_latex[subdir]+": "+str(time_print)+" yr, "+str(z_print)+" km, "+str(prs_print)+" bar, "+str(tmp_print)+" K"
-            # Label with time, height, pressure
-            label_a = vol_latex[subdir]+": "+str(z_print)+" km, "+str(prs_print)+" bar"
-
-            # Plot a line at the max location
-            print(cff_tot[cff_max_idx], prs[cff_max_idx], prs[cff_max_idx])
-            if output_label == "max":
-                ax0.plot([cff_tot[cff_max_idx], cff_tot[cff_max_idx]], [3e-5, prs[cff_max_idx]], ls="--", lw=lw/2., color=color, alpha=0.8)
-            if output_label == "weighted":
-                ax0.plot([0.081, 1], [prs_print/np.max(atm.p/1e+5), prs_print/np.max(atm.p/1e+5)], ls="--", lw=lw/2., color=color, alpha=0.8)
-
-            photosphere[subdir+"_prs_norm"] = prs_print/np.max(atm.p/1e+5)
-            photosphere[subdir+"_z"]        = z_print
-            photosphere[subdir+"_prs_bar"]  = prs_print
-
-            # Adjust x-axis right
-            xmax = np.amax([np.max(cff_tot), xmax])
-
-            # If smoothing
-            if nsmooth > 1:
-                cff_tot = np.convolve(cff_tot, np.ones((nsmooth,))/nsmooth, mode='valid')
-                prs     = np.convolve(prs, np.ones((nsmooth,))/nsmooth, mode='valid')
-
-
-            # Define data files
+            # Define data file
             data_file_cff = '../data/fig8_cff/'+str(subdir)+"_cff.txt"
 
-            if os.path.isfile(data_file_cff):
+            prs         = []
+            cff_tot     = []
+            cff_2       = []
+            cff_6       = []
+            cff_10      = []
+            cff_lambda  = {}
 
-                prs         = []
-                cff_tot     = []
-                cff_2       = []
-                cff_6       = []
-                cff_10      = []
-                cff_lambda  = {}
+            print("Read:", data_file_cff)
 
-                print("Read:", data_file_cff)
+            with open(data_file_cff, "r") as filestream:
+                next(filestream) # skip header
+                filestream = [line.rstrip('\n') for line in filestream]
+                for line in filestream:
+                    print(line)
+                    data_values = line.split(" ")
+                    cff_tot.append(float(data_values[0]))
+                    prs.append(float(data_values[1]))
+                    cff_2.append(float(data_values[2]))
+                    cff_6.append(float(data_values[3]))
+                    cff_10.append(float(data_values[4]))
 
-                with open(data_file_cff, "r") as filestream:
-                    next(filestream) # skip header
-                    filestream = [line.rstrip('\n') for line in filestream]
-                    for line in filestream:
-                        print(line)
-                        data_values = line.split(" ")
-                        cff_tot.append(float(data_values[0]))
-                        prs.append(float(data_values[1]))
-                        cff_2.append(float(data_values[2]))
-                        cff_6.append(float(data_values[3]))
-                        cff_10.append(float(data_values[4]))
+            cff_lambda["2"]     = cff_2
+            cff_lambda["6"]     = cff_6
+            cff_lambda["10"]    = cff_10    
 
-                cff_lambda["2"]     = cff_2
-                cff_lambda["6"]     = cff_6
-                cff_lambda["10"]    = cff_10
+            print(type(photosphere))
+            print(photosphere["H2_z"])
 
-            else:
-                cff_lambda = {}
-        
+            label_a = vol_latex[subdir]+": "+str(photosphere[subdir+"_z"])+" km, "+str(photosphere[subdir+"_prs_bar"])+" bar"  
 
-            # PLOT THE CFF
-            # l1, = ax0.semilogy(atm.cff/np.sum(atm.cff), atm.p/np.max(atm.p), ls=ls, lw=lw, color=color, label=label_a)
             l1, = ax0.semilogy(cff_tot, prs, ls=ls, lw=lw, color=color, label=label_a)
             legend_ax0_handles.append(l1)
 
-            wavelength_bands = [ 2, 6, 10 ] # microns
-
+            ax0.plot([0.081, 1], [photosphere[subdir+"_prs_norm"], photosphere[subdir+"_prs_norm"]], ls="--", lw=lw/2., color=color, alpha=0.8)
             
-
+            wavelength_bands = [ 2, 6, 10 ] # microns
             for w_idx, wavelength in enumerate(wavelength_bands):
 
                 if w_idx == 0:
@@ -330,44 +164,11 @@ def plot_atmosphere( output_dir, sub_dirs ):
                     lw = 1.5
                     ls = (0, (2, 1))
 
-                if wavelength == 1.65:
-                    band_name = r"$H$"
-                elif wavelength == 2:
-                    band_name = r"$K$"
-                elif wavelength == 3.55:
-                    band_name = r"$L$"
-                elif wavelength == 10:
-                    band_name = r"$N$"
-                else:
-                    band_name = "?"
-
                 wavenumber = (1./wavelength)*1e+4 # cm
 
                 label_b = vol_latex[subdir]+", "+str(round(wavelength, 1))+" $\mu$m"#+band_name
 
-                channel, channel_idx = find_nearest(atm.band_centres, wavenumber )
-
-                print(wavenumber, w_idx, wavelength, channel, channel_idx)
-                print(subdir, sub_dirs[0])
-
-                if os.path.isfile(data_file_cff):
-
-                    # print(cff_lambda)
-
-                    cff_band = cff_lambda[str(wavelength)]
-
-                else:
-
-                    # CFF per band
-                    cff_band = atm.cff_i[channel_idx,:]/np.sum(atm.cff_i[channel_idx,:])
-                    prs     = atm.p/np.max(atm.p)
-
-                    # If smoothing
-                    if nsmooth > 1:
-                        cff_band = np.convolve(cff_band, np.ones((nsmooth,))/nsmooth, mode='valid')
-                        prs     = np.convolve(prs, np.ones((nsmooth,))/nsmooth, mode='valid')
-
-                    cff_lambda[str(wavelength)] = cff_band.tolist()
+                cff_band = cff_lambda[str(wavelength)]
 
                 l2, = ax1.semilogy(cff_band, prs, ls=ls, lw=lw, color=color, label=label_b) # 
                 legend_ax1_handles.append(l2)
@@ -376,28 +177,6 @@ def plot_atmosphere( output_dir, sub_dirs ):
                 if subdir == sub_dirs[0]:
                     dummy, = ax1.semilogy([0], [0], ls=ls, lw=lw, color="k", label=str(round(wavelength, 1))+" $\mu$m") 
                     legend_ax1_dummy_handles.append(dummy)
-
-            print(cff_lambda)
-
-            if not os.path.isfile(data_file_cff):
-
-                ## Write to file
-                with open(data_file_cff, 'a') as data_file:
-                    data_file.write("# P CFF_tot CFF_2 CFF_6 CFF_10\n")
-                    for idx, cff in enumerate(cff_tot):
-                        data_file.write(str(cff)+" "+str(prs[idx])+" "+str(cff_lambda["2"][idx])+" "+str(cff_lambda["6"][idx])+" "+str(cff_lambda["10"][idx])+"\n")
-
-            # print(atm.cff)
-            # print(np.shape(atm.cff), np.shape(atm.LW_flux_up))
-            print("CFFs: ", np.sum(atm.cff), np.sum( atm.cff_i[:,:] ), atm.LW_flux_up[0], atm.net_flux[0])
-            cff_sum = np.sum( atm.cff_i[:,:] * atm.LW_flux_up_i[:,0][:,None], axis=0)
-            cff_sum = cff_sum / np.trapz(cff_sum, atm.p/np.max(atm.p), axis=0)
-            print(np.sum(cff_sum), atm.LW_flux_up[0])
-
-    if not os.path.isfile(data_file_photosphere):
-        a_file = open(data_file_photosphere, "w")
-        json.dump(photosphere, a_file)
-        a_file.close()
 
     ax0.set_xlabel( r'Normalized flux contribution, $\mathcal{CF}_\mathrm{F}$ (non-dim.)', fontsize=fs_label )
     ax0.invert_yaxis()
